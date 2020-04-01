6a14b08e91a2cc6cd127b0fdcb02a24a98c11be4
Eduardo Calderon Rodriguez <educr6@gmail.com>
2020-04-01 15:24:16 -0400

    add the first argument to the argsparse

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import argparse
 
+
+
+
+def configureStocksCLI():
+    parser = argparse.ArgumentParser(description="Give stocks information from yahoo finance")
+    addStockTickerArgumentToArgsParser(parser)
+
+
+def addStockTickerArgumentToArgsParser(parser):
+    parser.add_argument("Stock", help="Ticker of the stock you want to check")
+
+
+
 def main():
     print("Hello World")
+
 
 if __name__ == "__main__":
     main()
